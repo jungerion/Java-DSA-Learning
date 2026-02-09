a112989acbbcfadb938cfab82eb20bdd164b7bb8
Aakrit <jungerion@gmail.com>
2026-02-09 22:19:26 +0545

    Update factorial recursion example

--- 03_Sorting/sorting_practice/recursionap/code/src/Factorial.java
+++ 03_Sorting/sorting_practice/recursionap/code/src/Factorial.java
@@ -1,30 +1,32 @@
 public class Factorial {
 
-    // Recursive factorial
-//    public static int calcFact(int n){
-//        if(n==1|| n==0){return 1;}
-//
-//
-//      int fact_nm1=  calcFact(n-1);
-//        int fact_n=n*fact_nm1;
-//      return fact_n;
-//    }
-//
-//    static void main(String[] args) {
-//        int n=5;
-//        int ans=calcFact(n);
-//        System.out.println(ans );
-//    }
+    //     Recursive factorial
+    public static int calcFact(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
 
-public static int calcFact(int n){
-    if(n==0||n==1){
-        return 1;
+
+        int fact_nm1 = calcFact(n - 1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
     }
-    return n*calcFact(n-1);
-}
 
     static void main(String[] args) {
-        System.out.println(calcFact(5));
+        int n = 5;
+        int ans = calcFact(n);
+        System.out.println(ans);
     }
-
 }
+//public static int calcFact(int n){
+//    if(n==0||n==1){
+//        return 1;
+//    }
+//    return n*calcFact(n-1);
+//}
+//
+//    static void main(String[] args) {
+//        System.out.println(calcFact(5));
+//    }
+//
+//}
