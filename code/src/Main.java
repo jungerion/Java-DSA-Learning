1f3ed227a8e887b8a738f7a5b3bc67e9bb045a09
Aakrit <jungerion@gmail.com>
2026-02-11 22:51:57 +0545

    Added Fibonacci and updated recursion practice

--- code/src/Main.java
+++ code/src/Main.java
@@ -45,19 +45,50 @@
 //}
 
 public class Main {
-    public static void printNumb(int n){
-        if(n==0){
-            return;
-        }
-        System.out.println(n);
-        printNumb(n-1);
+    // Sum of n numbers
+
+//    public static void printSum(int i, int n, int sum){
+//        if(i==n){   // Base case
+//            sum+=i;
+//            System.out.println(sum);  // Here, 15 is printed and returned
+//            return;
+//        }
+//        sum+=i;
+//        printSum(i+1,n,sum);
+//        System.out.println(i);
+//    }
+//    static void main(String[] args) {
+//    printSum(1,5,0);
+//    }
+
+    public static int sum(int n){
+        if(n == 1) return 1;   // base case
+        return n + sum(n - 1);
     }
 
-    static void main(String[] args) {
-        int n=5;
-        printNumb(n);
+    public static void main(String[] args){
+        System.out.println(sum(5));   // prints 15
     }
+
+
+//    public static void printNumb(int n){
+//        if(n==6){
+//            return;
+//        }
+//        System.out.println(n);
+//        printNumb(n+1);
+//    }
+//
+//    static void main(String[] args) {
+//        int n=1;
+//        printNumb(n);
+//    }
+
+
+
 }
+
+
 
 
 
